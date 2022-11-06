cfe18783bcf3cee1b9ac34648917aaab2d027414
Kit Daniel Lim <kitdaniellim@gmail.com>
2022-11-06 14:38:49 +0800

    Initial weather api config

--- src/views/pages/inside/Weather.js
+++ src/views/pages/inside/Weather.js
@@ -3,4 +3,6 @@ import '../../../App.css';
 
 export default function Weather() {
   return <h1 className='services'>Weather</h1>;
+//   http://api.openweathermap.org/geo/1.0/direct?q=Cebu&limit=5&appid=b264884b58d343c9b3d3da05ece5f2f5
+// https://api.openweathermap.org/data/2.5/weather?lat=-53.92572&lon=-93.55650&appid=b264884b58d343c9b3d3da05ece5f2f5
 }
